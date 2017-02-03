1af1547261fd3c7a427143692aefa813bfa3a4c4
Barudisshu <chemnk@163.com>
2017-02-03 22:57:29 +0800

    leading way

---
+++ fpp/fp1/src/main/scala/cn/galudisu/fp/_1_4_immutable/Immutable.scala
@@ -0,0 +1,14 @@
+package cn.galudisu.fp._1_4_immutable
+
+import com.typesafe.scalalogging.LazyLogging
+
+/**
+  * 不可变性(Immutable)
+  *
+  * @author galudisu
+  */
+object Immutable extends App with LazyLogging {
+
+
+
+}
